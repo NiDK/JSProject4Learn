2928d77434fe532df09641d40f9a5381fe093cb6
HelloWorld <nidakun@163.com>
2017-07-22 23:49:08 +0800

    V20170722
    node建站（一）删除功能前

--- app.js
+++ app.js
@@ -1,44 +1,140 @@
 var express = require('express')
+var path=require('path')
+var _=require('underscore')
+var mongoose =require("mongoose")
+var bodyParser=require('body-parser')
+var Movie =require('./models/movie')
 var port = process.env.PORT || 3000
 var app =express()
 
-app.set('views','./views')
+mongoose.connect('mongodb://localhost/Test')
+
+app.set('views','./views/pages')
 app.set('view engine','jade')
+
+app.use(bodyParser.urlencoded({extended:true}))
+app.use(express.static(path.join(__dirname,'bower_components')))
 app.listen(port)
 
 console.log('imooc started on port '+port);
 
 // index page
 app.get('/',function(req,res){
-	res.render('index',{
-		title:'imooc 首页'
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err);
+		}
+
+		res.render('index',{
+			title:'imooc 首页',
+			movies:movies
+		})
 	})
+	
 })
 
-// index page
-app.get('/',function(req,res){
-	res.render('detail',{
-		title:'imooc 详情页'
+// detail page
+app.get('/movie/:id',function(req,res){
+	var id=req.params.id
+
+	Movie.findById(id, function(err,movie){
+		if(err){
+			console.log(err)
+		}
+
+		res.render('detail',{
+		title:'imooc '+movie.title,
+		movie:movie
 	})
+	})
+
+	
 })
 
-// index page
-app.get('/',function(req,res){
+// admin page
+app.get('/admin/movie',function(req,res){
 	res.render('admin',{
-		title:'imooc 后台录入页'
+		title:'imooc 后台录入页',
+		movie:{
+			title:'',
+			doctor:'',
+			country:'',
+			year:'',
+			poster:'',
+			flash:'',
+			summary:'',
+			language:''
+		}
 	})
 })
 
-// index page
-app.get('/',function(req,res){
-	res.render('list',{
-		title:'imooc 列表页'
-	})
+//admin update movie
+app.get('/admin/update/:id',function(req,res){
+	var id=req.params.id
+
+	if(id){
+		Movie.findById(id,function(err,movie){
+			res.render('admin',{
+				title:'imooc 后台更新页',
+				movie:movie
+			})
+		})
+	}
+})
+
+// admin post movie
+app.post('/admin/movie/new',function(req,res){
+	var id=req.body.movie._id
+	var movieObj=req.body.movie
+	var _movie
+
+	if(id !=='undefined'){
+		Movie.findById(id,function(err,movie){
+			if(err){
+				console.log(err)
+
+			}
+
+			_movie=_.extend(movie,movieObj)
+			_movie.save(function(err,movie){
+				if(err){
+					console.log(err)
+				}
+				res.redirect('/movie/'+movie._id)
+			})
+		})
+	}else{
+		_movie=new Movie({
+			doctor:movieObj.doctor,
+			title:movieObj.title,
+			country:movieObj.country,
+			language:movieObj.language,
+			year:movieObj.year,
+			poster:movieObj.poster,
+			summary:movieObj.summary,
+			flash:movieObj.flash
+
+		})
+		_movie.save(function(err,movie){
+				if(err){
+					console.log(err)
+				}
+				res.redirect('/movie/'+movie._id)
+			})
+	}
 })
 
 // index page
-app.get('/',function(req,res){
-	res.render('index',{
-		title:'imooc 首页'
+app.get('/admin/list',function(req,res){
+	Movie.fetch(function(err,movies){
+		if(err){
+			console.log(err);
+		}
+
+		res.render('list',{
+			title:'imooc 首页',
+			movies:movies
+		})
 	})
 })
+
